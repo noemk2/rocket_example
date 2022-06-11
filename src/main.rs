fa00c97a404e1c7f7e6beaaa4b1722aa1d0cd1fc
Noe <noemk2@gmail.com>
2022-06-11 20:00:25 +0000

    fnal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use rocket_dyn_templates::{Template, context};
+
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context!{
+        title: "vista" ,  
+    } )
+}
+
+#[get("/hola")]
+fn hola() -> &'static str{
+    "twitter hola 2"
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount( "/", routes![index, hola]).attach(Template::fairing())
 }
